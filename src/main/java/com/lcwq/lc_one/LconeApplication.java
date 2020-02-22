d4bcbba2576a3ab6c34ad94d52fa14a78d93582b
asdfggqaz <liangc4321@gmail.com>
2020-02-22 14:33:25 +0800

    first commit

---
+++ src/main/java/com/lcwq/lc_one/LconeApplication.java
@@ -0,0 +1,13 @@
+package com.lcwq.lc_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LconeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LconeApplication.class, args);
+    }
+
+}
